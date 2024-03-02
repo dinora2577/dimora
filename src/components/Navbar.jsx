b6feb2da449d610941d6c0163ca7bae1f6b24abb
dinora2577 <educationjumanazar@gmail.com>
2024-03-02 12:04:56 +0500

    navbar section end
    navbar section end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiMiniBars2 } from "react-icons/hi2";
 
 const Navbar = () => {
+    const[toggle,setToggle] =useState(false)
+    console.log(toggle);
   return (
    
 
-    <nav className=' containerr w-full h-[80px] bg-[#4285c7] flex justtify-between'>
-        <div>
+    <nav className=' containerr fixed w-full h-[80px] backdrop-blur flex justify-between items-center'>
+        <div className='text-[30px] font-bold'>
             Dinora.
         </div>
         <div>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Skills</a></li>
-                <li><a href="#">My Works</a></li>
-                <li><a href="#">Contact</a></li>
+            <ul className={`flex md:flex-row flex-col gap-6 text-[#6b7688] font-[600] uppercase md:bg-[transparent] md:w-auto md:static bg-[white]  w-[80%] ${toggle ? 'right-0' : 'right-[-100%]'} top-[80px] md:h-auto h-[100vh] absolute transition-[1s] md:p-0 p-10 text-[14px]`} >
+                <li><a href="#" className='links'>Home</a></li>
+                <li><a href="#" className='links'>Skills</a></li>
+                <li><a href="#" className='links'>My Works</a></li>
+                <li><a href="#" className='links'>Contact</a></li>
             </ul>
         </div>
-        <div>
+
+        <div className='flex gap-5 ' >
             <div>RU</div>
             <div>ENG</div>
         </div>
-
+        {/* /*bars/* */}
+        <div className='p-1 bg-[#313bac] text-[white] rounded-full text-[25px] md:hidden block' onClick={()=>setToggle(!toggle)}>
+        
+        <HiMiniBars2/> 
+        </div>
     </nav>
   )
 }
