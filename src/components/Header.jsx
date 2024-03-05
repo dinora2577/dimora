2d4a047fd46af7f4316dc59d368f8ac84f3274f5
dinora2577 <educationjumanazar@gmail.com>
2024-03-05 11:09:38 +0500

    navbar simple
    navbar simple

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        {/* download cv */}
+        <div>
+          <div className='flex justify-center items-center gap-5 w-full max-w-[200px] text-center rounded-[10px] shadow-lg '>
+            <p>👋</p>
+            <p className='flex flex-col'>
+                <span>Hello,I am</span>
+                <span>Dinora</span>
+            </p>
+            </div> 
+          <div></div> 
+          <button></button>
+
+        </div>
+
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Header
